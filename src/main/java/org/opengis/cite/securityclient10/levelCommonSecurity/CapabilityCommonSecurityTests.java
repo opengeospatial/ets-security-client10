3466e4d0c19be0315ad7e90e5161634643091717
James Badger <james@jamesbadger.ca>
2018-09-09 16:18:19 -0600

    Add test method for HTTPS

--- src/main/java/org/opengis/cite/securityclient10/levelCommonSecurity/CapabilityCommonSecurityTests.java
+++ src/main/java/org/opengis/cite/securityclient10/levelCommonSecurity/CapabilityCommonSecurityTests.java
@@ -1,19 +1,30 @@
 package org.opengis.cite.securityclient10.levelCommonSecurity;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
 import org.opengis.cite.securityclient10.CommonFixture;
-import org.opengis.cite.securityclient10.ErrorMessage;
-import org.opengis.cite.securityclient10.ErrorMessageKeys;
 import org.opengis.cite.securityclient10.SuiteAttribute;
 import org.testng.Assert;
 import org.testng.ITestContext;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 /**
  * Includes various tests of Common Security capability.
  */
 public class CapabilityCommonSecurityTests extends CommonFixture {
 
+	private Document testRequestSet;
     private String testServiceType;
 
     /**
@@ -34,33 +45,44 @@ public class CapabilityCommonSecurityTests extends CommonFixture {
             this.testServiceType = String.class.cast(obj);
         }
     }
-
+    
     /**
-     * Sets the test service type. This method is intended to facilitate unit
-     * testing.
-     *
-     * @param testServiceType A String representing an OGC Web Service type.
+     * Obtain the Implementation Under Test (IUT) from the ISuite context. This is a path to a file
+     * containing the serialized request(s) made by the secure client. See 
+     * org.opengis.cite.securityclient10.httpServer.RequestRepresenter for XML structure details.
+     * 
+     * @param testContext The test (group) context.
+     * @throws ParserConfigurationException Exception for failure to create a DocumentBuilder.
+     * @throws IOException Exception when reading the IUT file from disk.
+     * @throws SAXException Exception when parsing the IUT file.
      */
-    public void setTestServiceType(String testServiceType) {
-        this.testServiceType = testServiceType;
+    @BeforeClass
+    public void obtainIUT(ITestContext testContext) throws ParserConfigurationException, SAXException, IOException {
+        Object obj = testContext.getSuite().getAttribute(SuiteAttribute.TEST_IUT.getName());
+        if (null != obj) {
+            String iutPath = String.class.cast(obj);
+            File iutFile = new File(iutPath);
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            this.testRequestSet = db.parse(iutFile);
+        }
     }
 
     /**
-     * Verifies the string is empty.
+     * Verifies that the secure client connects over HTTPS.
+     * Requirements Class: urn:ogc:def:security:1.0:rc:https
      */
-    @Test(description = "Implements ATC 1-1")
-    public void isEmpty() {
-        String str = "  foo   ";
-        Assert.assertTrue(str.isEmpty(),
-                ErrorMessage.get(ErrorMessageKeys.EMPTY_STRING));
-    }
-
-    /**
-     * Checks the behavior of the trim function.
-     */
-    @Test(description = "Implements ATC 1-2")
-    public void trim() {
-        String str = "  foo   ";
-        Assert.assertTrue("foo".equals(str.trim()));
+    @Test(description = "Connection(s) made using HTTPS")
+    public void isHTTPS() {
+        NodeList requests = this.testRequestSet.getElementsByTagName("Request");
+        
+        for (int i = 0; i < requests.getLength(); i++) {
+        	Node request = requests.item(i);
+        	
+        	if (request.getNodeType() == Node.ELEMENT_NODE) {
+        		Element requestElement = (Element) request;
+        		Assert.assertEquals(requestElement.getAttribute("https"), "true", "A request was sent without HTTPS.");
+        	}
+        }
     }
 }
