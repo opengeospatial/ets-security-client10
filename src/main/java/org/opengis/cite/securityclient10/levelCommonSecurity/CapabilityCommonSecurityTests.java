b5227bb7d03725c3355d61f8315456d4b10c4341
James Badger <james@jamesbadger.ca>
2018-09-09 20:28:07 -0600

    Move manual checklist test to conformance class
    It should be in the test methods instead of the TestNG setup
    functions.

--- src/main/java/org/opengis/cite/securityclient10/levelCommonSecurity/CapabilityCommonSecurityTests.java
+++ src/main/java/org/opengis/cite/securityclient10/levelCommonSecurity/CapabilityCommonSecurityTests.java
@@ -85,4 +85,39 @@ public class CapabilityCommonSecurityTests extends CommonFixture {
         	}
         }
     }
+    
+    /**
+     * Conformance Test: Working on Capabilities with no Content section
+     * Requirements Class: https://www.opengis.net/def/security/1.0/cr/clientParsing/3
+     * 
+     * This is a manual test that must be verified by the test operator.
+     */
+    @Test(description = "Capabilities document with no Content section")
+    public void noContentSectionCapabilities() {
+    	// Print out Client Conformance Test Checklist for human operator
+        System.out.println("\nClient Conformance Test **Manual** Checklist");
+        System.out.println("=================================");
+        System.out.println("1. Conformance Test \"HTTPS\"");
+        System.out.println("Does the client parse the HTTPS response from the test suite?");
+        System.out.println("YES - Pass");
+        System.out.println("NO  - Failure");
+        System.out.println("");
+        System.out.println("2. Conformance Test \"Working on Capabilities with no Content section\"");
+        System.out.println("Does the test suite Capabilities contain a <Content> section?");
+        System.out.println("(Note that the \"Content\" section is represented as \"<Layer>\" in WMS,");
+        System.out.println("see Section 8.2.2 of v1.0 of the specification for other services.)");
+        System.out.println("YES - Skip Test");
+        System.out.println("NO  - Continue");
+        System.out.println("Does the test suite Capabilities contain an endpoint URL for the GetCapabilities operation?");
+        System.out.println("YES - Continue");
+        System.out.println("NO  - Failure");
+        System.out.println("Does the GetCapabilities operation on that endpoint URL succeed?");
+        System.out.println("YES - Continue");
+        System.out.println("NO  - Failure");
+        System.out.println("Does the Capabilities document contain a <Content> section?");
+        System.out.println("YES - Pass");
+        System.out.println("NO  - Failure");
+        System.out.println("");
+        System.out.println("If either Conformance Test went to a \"Failure\" option, then that Test should be marked as a \"Failure\".");
+    }
 }
